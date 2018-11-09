cd294496115fab4ed0bba204f021fd3faedd8437
Ning Wei <ning.wei@int-trade.de>
2018-11-09 15:41:55 +0100

    create AppBundle\User.php, protected $id, migration

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nwe
+ * Date: 09.11.18
+ * Time: 15:10
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
